eb911aa6267697878b53119b780f9572748bc4c2
Kevin Pennarun <kevin.pennarun@gmail.com>
2026-03-06 15:49:37 +0100

    feat: upgrade chezmoi config (#2)

---
+++ dot_config/zsh/completion.zsh
@@ -0,0 +1,16 @@
+# Zsh completion setup
+
+autoload -Uz compinit
+compinit -C
+
+# zsh-autosuggestions
+[ -f /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ] && \
+    source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ] && \
+    source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh-syntax-highlighting (must be sourced last among plugins)
+[ -f /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && \
+    source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+[ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && \
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
